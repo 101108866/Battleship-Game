4a692f0f5e31af41f327fc13c28b4b6b60049753
mitreyn <101109966@swin.edu.au>
2016-10-11 22:28:01 +1100

    fix-score
    Score bug is now fixed

--- src/Model/Player.cs
+++ src/Model/Player.cs
@@ -180,17 +180,19 @@ public class Player : IEnumerable<Ship>
 	/// <returns>the result of the attack</returns>
 	internal AttackResult Shoot(int row, int col)
 	{
-		_shots += 1;
+		
 		AttackResult result = default(AttackResult);
 		result = EnemyGrid.HitTile(row, col);
 
 		switch (result.Value) {
 			case ResultOfAttack.Destroyed:
 			case ResultOfAttack.Hit:
-				_hits += 1;
+                _shots += 1;
+                _hits += 1;
 				break;
 			case ResultOfAttack.Miss:
-				_misses += 1;
+                _shots += 1;
+                _misses += 1;
 				break;
 		}
 
